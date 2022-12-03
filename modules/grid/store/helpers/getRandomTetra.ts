8c3232b2a8120309f647321c26061ee87fe07d4a
Alexey Tuzov <sugarfox.dev@gmail.com>
2022-12-03 18:04:01 +0300

    transform dict to num array base

--- modules/grid/store/helpers/getRandomTetra.ts
+++ modules/grid/store/helpers/getRandomTetra.ts
@@ -1,51 +1,59 @@
 import { Position, Tetra } from "../types"
 
-enum TetrasDictionary {
-  L = `
-    10
-    10
-    11
-  `,
-  P = `
-    11
-    10
-    10
-  `,
-  Z = `
-    110
-    011
-  `,
-  S = `
-    011
-    110
-  `,
-  I = `
-    1
-    1
-    1
-    1
-  `,
-  T = `
-    010
-    111
-  `,
-  O = `
-    11
-    11
-  `
+export enum TetraType {
+  L = 'L',
+  P = 'P',
+  Z = 'Z',
+  S = 'S',
+  I = 'I',
+  T = 'T',
+  O = 'O',
 }
 
-function dictToTetra(val: string): Tetra {
+const TetrasDictionary: Record<TetraType, Array<number[]>> = {
+  L: [
+    [1, 0],
+    [1, 0],
+    [1, 1],
+  ],
+  P: [
+    [1, 1],
+    [1, 0],
+    [1, 0],
+  ],
+  Z: [
+    [1, 1, 0],
+    [0, 1, 1],
+  ],
+  S: [
+    [0, 1, 1],
+    [1, 1, 0],
+  ],
+  I: [
+    [1],
+    [1],
+    [1],
+    [1],
+  ],
+  T: [
+    [0, 1, 0],
+    [1, 1, 1],
+  ],
+  O: [
+    [1, 1],
+    [1, 1],
+  ]
+}
+
+function dictToTetra(rows: Array<number[]>): Tetra {
   const result: Position[] = []
 
-  const rows = val.replaceAll(' ', '').split('\n').filter(Boolean)
-
-  for (const [rowIndex, row] of Object.entries(rows)) {
-    for (const [cellIndex, cell] of Object.entries(row)) {
-      if (!Number(cell)) continue
-      result.push({ x: Number(cellIndex), y: Number(rowIndex) })
-    }
-  }
+  rows.forEach((row, rowIndex) => {
+    row.forEach((col, colIndex) => {
+      if (!col) return
+      result.push({ x: colIndex, y: rowIndex })
+    })
+  })
 
   return result as Tetra
 }
