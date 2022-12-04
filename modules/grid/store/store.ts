2d51e0ca4ee1e9dc4c1df38923b0b5dd4f702b26
Alexey Tuzov <sugarfox.dev@gmail.com>
2022-12-04 20:44:31 +0300

    add grid store state setters

--- modules/grid/store/store.ts
+++ modules/grid/store/store.ts
@@ -1,9 +1,11 @@
 import create from 'zustand'
 import { immer } from 'zustand/middleware/immer'
-import { GridStore } from './types'
+import { GridCell, GridStore, PositionId, Tetra } from './types'
 import { getTetra } from './helpers'
 
 export const useGridStore = create<GridStore>()(immer((set) => ({
   cells: {},
-  tetras: [getTetra(), getTetra()]
+  tetras: [getTetra(), getTetra()],
+  setTetra: (index: 0 | 1, tetra: Tetra) => set(state => { state.tetras[index] = tetra }),
+  setCell: (id: PositionId, cell: GridCell) => set(state => { state.cells[id] = cell })
 })))
