a89382351fd3e09cf8c77512c0d45ac32dc9e132
Alexey Tuzov <sugarfox.dev@gmail.com>
2022-12-07 21:03:24 +0300

    add default fallbacks to box shadow generation util

--- utils/styles/boxShadow.ts
+++ utils/styles/boxShadow.ts
@@ -1,17 +1,25 @@
 import { Platform, ViewStyle } from "react-native"
 
 export interface GenerateBoxShadowStyleOptions {
-  xOffset: number
-  yOffset: number
-  shadowColorIos: string
-  shadowOpacity: number
-  shadowRadius: number
-  elevation: number
-  shadowColorAndroid: string
+  elevation?: number
+  shadowColorAndroid?: string
+  shadowColorIos?: string
+  shadowOpacity?: number
+  shadowRadius?: number
+  xOffset?: number
+  yOffset?: number
 }
 
 export function generateBoxShadowStyle(options: GenerateBoxShadowStyleOptions): ViewStyle {
-  const { shadowColorAndroid, shadowColorIos, shadowOpacity, shadowRadius, elevation, yOffset, xOffset } = options
+  const {
+    elevation = 0,
+    shadowColorAndroid = 'black',
+    shadowColorIos = 'black',
+    shadowOpacity = 1,
+    shadowRadius = 0,
+    xOffset = 0,
+    yOffset = 0,
+  } = options
 
   if (Platform.OS === 'ios') return {
     shadowColor: shadowColorIos,
