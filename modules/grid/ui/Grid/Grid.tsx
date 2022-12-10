91198fbe9ee9c538f041712169edd149ec7d8398
Alexey Tuzov <sugarfox.dev@gmail.com>
2022-12-10 19:42:58 +0300

    setup grid display with proper touch detection

--- modules/grid/ui/Grid/Grid.tsx
+++ modules/grid/ui/Grid/Grid.tsx
@@ -1,48 +1,19 @@
-import {
-  GestureResponderEvent,
-  LayoutChangeEvent,
-  LayoutRectangle,
-  StyleSheet,
-  TouchableOpacity,
-  View
-} from 'react-native'
 import React, { useRef } from "react"
+import { GestureResponderEvent, LayoutRectangle, StyleSheet, LayoutChangeEvent, View, useWindowDimensions } from "react-native"
+import { Position, isWithin, PositionId, positionToId } from "~/modules/position"
+import { Cell } from "~/modules/tetra/ui/Cell"
 
-const styles = StyleSheet.create({
-  container: {
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-    justifyContent: 'center'
-  },
-  item: {
-    margin: 16,
-    backgroundColor: 'red',
-    width: 100,
-    height: 100
-  }
-})
-
-const children = [
-  <TouchableOpacity key={0} style={styles.item} />,
-  <TouchableOpacity key={1} style={styles.item} />,
-  <TouchableOpacity key={2} style={styles.item} />,
-  <TouchableOpacity key={3} style={styles.item} />
-]
-
-interface Pos {
-  x: number
-  y: number
-}
-
-function isWithin(pos: Pos, target: LayoutRectangle): boolean {
-  if (!(target.x < pos.x && target.x + target.width > pos.x)) return false
-  return target.y < pos.y && target.y + target.width > pos.y
-}
+const GRID_WIDTH = 10
+const GRID_HEIGHT = 10
 
 export function Grid() {
+  const { width } = useWindowDimensions()
+
+  const cellSize = width / 10
+
   function handleMove(e: GestureResponderEvent) {
-    const { pageX: x, pageY: y } = e.nativeEvent
-    const pos: Pos = { x, y }
+    const { locationX: x, locationY: y } = e.nativeEvent
+    const pos: Position = { x, y }
 
     for (const [id, item] of Object.entries(itemLayouts.current)) {
       if (!isWithin(pos, item)) continue
@@ -53,17 +24,36 @@ export function Grid() {
     }
   }
 
-  const itemLayouts = useRef<Record<string, LayoutRectangle>>({})
+  const itemLayouts = useRef<Record<PositionId, LayoutRectangle>>({})
 
-  function onLayout(e: LayoutChangeEvent, id: string) {
+  function handleCellLayout(e: LayoutChangeEvent, id: PositionId) {
     itemLayouts.current[id] = e.nativeEvent.layout
   }
 
-  const mappedChildren = children.map((child, index) => React.cloneElement(child, { onLayout: (e: LayoutChangeEvent) => onLayout(e, String(index)), key: index }))
-
   return (
-    <View style={styles.container} onTouchMove={handleMove}>
-      {mappedChildren}
+    <View style={styles.wrapper}>
+      {[...Array(GRID_HEIGHT)].map((_, y) => (
+        [...Array(GRID_WIDTH)].map((_, x) => (
+          <Cell key={positionToId({ x, y })} size={cellSize} onLayout={(e) => handleCellLayout(e, positionToId({ x, y }))} />
+        ))
+      ))}
+
+      <View style={styles.hitbox} onTouchStart={handleMove} onTouchMove={handleMove} />
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  wrapper: {
+    position: 'relative',
+    width: '100%',
+    aspectRatio: 1,
+    flexDirection: 'row',
+    flexWrap: 'wrap'
+  },
+  hitbox: {
+    position: 'absolute',
+    width: '100%',
+    height: '100%'
+  }
+})
