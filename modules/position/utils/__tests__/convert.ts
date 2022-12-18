86aab7862218a88f110700d4534975ce35fec0aa
Alexey Tuzov <sugarfox.dev@gmail.com>
2022-12-18 12:15:05 +0300

    update tests

--- modules/position/utils/__tests__/convert.ts
+++ modules/position/utils/__tests__/convert.ts
@@ -3,10 +3,13 @@ import { idToPosition, positionToId, PositionId, Position } from '../..'
 describe('convert', () => {
   describe('idToPosition', () => {
     it.each<[Position, PositionId]>([
-      [{ x: 10, y: 10 }, '10-10'],
-      [{ x: 12, y: 8 }, '12-8'],
-      [{ x: 100, y: 0 }, '100-0'],
-      [{ x: 15, y: 150 }, '15-150']
+      [{ x: 10, y: 10 }, '10/10'],
+      [{ x: 12, y: 8 }, '12/8'],
+      [{ x: 100, y: 0 }, '100/0'],
+      [{ x: 15, y: 150 }, '15/150'],
+      [{ x: -15, y: 15 }, '-15/15'],
+      [{ x: 15, y: -15 }, '15/-15'],
+      [{ x: -15, y: -15 }, '-15/-15'],
     ])('should convert %p to %s', (input, output) => {
       expect(positionToId(input)).toEqual(output)
     })
@@ -14,10 +17,13 @@ describe('convert', () => {
 
   describe('positionToId', () => {
     it.each<[PositionId, Position]>([
-      ['10-10', { x: 10, y: 10 }],
-      ['12-8', { x: 12, y: 8 }],
-      ['100-0', { x: 100, y: 0 }],
-      ['15-150', { x: 15, y: 150 }],
+      ['10/10', { x: 10, y: 10 }],
+      ['12/8', { x: 12, y: 8 }],
+      ['100/0', { x: 100, y: 0 }],
+      ['15/150', { x: 15, y: 150 }],
+      ['-15/15', { x: -15, y: 15 }],
+      ['15/-15', { x: 15, y: -15 }],
+      ['-15/-15', { x: -15, y: -15 }],
     ])('should convert %s to %p', (input, output) => {
       expect(idToPosition(input)).toEqual(output)
     })
