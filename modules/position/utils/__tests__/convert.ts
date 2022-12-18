08f3519a88432cb1a59faa2eb9e8c6e5e6ea7929
Alexey Tuzov <sugarfox.dev@gmail.com>
2022-12-18 12:12:49 +0300

    add position conversion tests

--- modules/position/utils/__tests__/convert.ts
+++ modules/position/utils/__tests__/convert.ts
@@ -1,9 +1,25 @@
+import { idToPosition, positionToId, PositionId, Position } from '../..'
+
 describe('convert', () => {
   describe('idToPosition', () => {
-    it.todo('should convert %p to %s')
+    it.each<[Position, PositionId]>([
+      [{ x: 10, y: 10 }, '10-10'],
+      [{ x: 12, y: 8 }, '12-8'],
+      [{ x: 100, y: 0 }, '100-0'],
+      [{ x: 15, y: 150 }, '15-150']
+    ])('should convert %p to %s', (input, output) => {
+      expect(positionToId(input)).toEqual(output)
+    })
   })
 
   describe('positionToId', () => {
-    it.todo('should convert %s to %p')
+    it.each<[PositionId, Position]>([
+      ['10-10', { x: 10, y: 10 }],
+      ['12-8', { x: 12, y: 8 }],
+      ['100-0', { x: 100, y: 0 }],
+      ['15-150', { x: 15, y: 150 }],
+    ])('should convert %s to %p', (input, output) => {
+      expect(idToPosition(input)).toEqual(output)
+    })
   })
 })
