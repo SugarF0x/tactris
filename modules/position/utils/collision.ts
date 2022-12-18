fc3887a9ab6ae44844a4e64c716df7d62da13e5e
Alexey Tuzov <sugarfox.dev@gmail.com>
2022-12-18 12:08:02 +0300

    allow positions on the border itself

--- modules/position/utils/collision.ts
+++ modules/position/utils/collision.ts
@@ -2,6 +2,6 @@ import { Position } from "../types"
 import { LayoutRectangle } from "react-native"
 
 export function isWithin(pos: Position, target: LayoutRectangle): boolean {
-  if (!(target.x < pos.x && target.x + target.width > pos.x)) return false
-  return target.y < pos.y && target.y + target.height > pos.y
+  if (!(target.x <= pos.x && target.x + target.width >= pos.x)) return false
+  return target.y <= pos.y && target.y + target.height >= pos.y
 }
