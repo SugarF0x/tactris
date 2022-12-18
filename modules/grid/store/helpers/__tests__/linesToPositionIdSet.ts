fbe06eb75b5c3cf93bccbfb9b46b5b5eb24c9a5b
Alexey Tuzov <sugarfox.dev@gmail.com>
2022-12-18 23:24:17 +0300

    add linesToPositionIdSet tests

--- modules/grid/store/helpers/__tests__/linesToPositionIdSet.ts
+++ modules/grid/store/helpers/__tests__/linesToPositionIdSet.ts
@@ -1,3 +1,29 @@
+import { linesToPositionIdSet } from '../linesToPositionIdSet'
+import { CompletionLine, GRID_HEIGHT, GRID_WIDTH } from "~/modules/grid"
+import { Axis, PositionId, positionToId } from "~/modules/position"
+
 describe('linesToPositionIdSet', () => {
-  it.todo("should return all completion lines' position IDs")
+  it.each<[CompletionLine[], PositionId[]]>([
+    [[], []],
+    [
+      [{ axis: Axis.X, value: 0 }],
+      Array.from({ length: GRID_HEIGHT }, (_, y) => positionToId({ x: 0, y }))
+    ],
+    [
+      [{ axis: Axis.X, value: 1 }, { axis: Axis.X, value: 2 }],
+      [
+        ...Array.from({ length: GRID_HEIGHT }, (_, y) => positionToId({ x: 1, y })),
+        ...Array.from({ length: GRID_HEIGHT }, (_, y) => positionToId({ x: 2, y })),
+      ]
+    ],
+    [
+      [{ axis: Axis.X, value: 3 }, { axis: Axis.Y, value: 3 }],
+      [
+        ...Array.from({ length: GRID_HEIGHT }, (_, y) => positionToId({ x: 3, y })),
+        ...Array.from({ length: GRID_WIDTH }, (_, x) => x === 3 ? null : positionToId({ x, y: 3 })).filter((e): e is PositionId => Boolean(e))
+      ]
+    ]
+  ])("should return all completion lines' position IDs %#", (input, output) => {
+    expect(linesToPositionIdSet(input)).toEqual(expect.arrayContaining(output))
+  })
 })
