4db0759878659e2ca38a42e43d82212882b69f3b
Alexey Tuzov <sugarfox.dev@gmail.com>
2022-12-18 12:17:37 +0300

    add guard tests

--- modules/position/utils/__tests__/guards.ts
+++ modules/position/utils/__tests__/guards.ts
@@ -1,6 +1,24 @@
+import { isPositionId } from '../guards'
+
 describe('guards', () => {
   describe('isPositionId', () => {
-    it.todo('should validate %s')
-    it.todo('should invalidate %s')
+    it.each<string>([
+      '10/10',
+      '100/0',
+      '-10/10',
+      '10/-10',
+      '-10/-10',
+    ])('should validate %s', (input) => {
+      expect(isPositionId(input)).toBeTruthy()
+    })
+
+    it.each<string>([
+      '10/A',
+      'asd/29',
+      'asd/ad',
+      'asdgsfd',
+    ])('should invalidate %s', (input) => {
+      expect(isPositionId(input)).not.toBeTruthy()
+    })
   })
 })
