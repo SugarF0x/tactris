2810b1401fdc226b86d7e3146eec4bdcc28b0af4
Alexey Tuzov <sugarfox.dev@gmail.com>
2022-12-18 23:04:00 +0300

    add getFilledLines tests

--- modules/grid/store/helpers/__tests__/getFilledLines.ts
+++ modules/grid/store/helpers/__tests__/getFilledLines.ts
@@ -1,3 +1,22 @@
+import { Axis, PositionId, positionToId } from "~/modules/position"
+import { CompletionLine, GRID_HEIGHT, GRID_WIDTH } from "~/modules/grid"
+import { getFilledLines } from '../getFilledLines'
+
 describe('getFilledLines', () => {
-  it.todo('return given lines that have all their ids filled in given position IDs')
+  it.each<[PositionId[], CompletionLine[], CompletionLine[]]>([
+    [[], [{ axis: Axis.X, value: 0 }], []],
+    [['0/0'], [{ axis: Axis.X, value: 0 }, { axis: Axis.Y, value: 0 }], []],
+    [
+      Array.from({ length: GRID_HEIGHT }, (_, y) => positionToId({ x: 0, y })),
+      [{ axis: Axis.X, value: 0 }, { axis: Axis.Y, value: 0 }],
+      [{ axis: Axis.X, value: 0 }]
+    ],
+    [
+      Array.from({ length: GRID_WIDTH }, (_, x) => positionToId({ x, y: 0 })),
+      [{ axis: Axis.X, value: 0 }, { axis: Axis.Y, value: 0 }],
+      [{ axis: Axis.Y, value: 0 }]
+    ],
+  ])('return given lines that have all their ids filled in given position IDs %#', (ids, lines, output) => {
+    expect(getFilledLines(ids, lines)).toEqual(expect.arrayContaining(output))
+  })
 })
