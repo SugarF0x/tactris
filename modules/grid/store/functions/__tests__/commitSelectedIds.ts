5fdd63e7f8c2715db12bd7c97006dd2d8322dcb3
Alexey Tuzov <sugarfox.dev@gmail.com>
2022-12-25 00:11:36 +0300

    add failing test case to be fixed

--- modules/grid/store/functions/__tests__/commitSelectedIds.ts
+++ modules/grid/store/functions/__tests__/commitSelectedIds.ts
@@ -1,7 +1,9 @@
 import { commitSelectedIds } from "~/modules/grid/store/functions"
 import { getGridStoreInitialStateMock } from "./mocks"
 import { getSpecificTetra, TetraObject, TetraType } from "~/modules/tetra"
-import { PositionId } from "~/modules/position"
+import { PositionId, positionToId } from "~/modules/position"
+import { GRID_WIDTH } from "~/modules/grid"
+import * as gridConfig from "~/modules/grid/config"
 
 describe('commitSelectedIds', () => {
   it('should do early return on selected IDs length < 4 (%#)', () => {
@@ -55,5 +57,37 @@ describe('commitSelectedIds', () => {
 
     expect(draft.filledIds).toEqual(expect.arrayContaining(selectedIds))
     expect(draft.selectedIds).toHaveLength(0)
+  })
+
+  it('should properly shift specified items from both sides', () => {
+    const initialGridConfig = { ...gridConfig }
+    Object.assign(gridConfig, { GRID_WIDTH: 10, GRID_HEIGHT: 10 })
+
+    const almostFilledLines = [...Array(4)].flatMap((_, y) => [...Array(GRID_WIDTH - 1)].map((_, x) => positionToId({ x, y: y + 3 })))
+    const fillSelection = [...Array(4)].map((_, y) => positionToId({ x: GRID_WIDTH - 1, y: y + 3 }))
+
+    const miscCells: PositionId[] = [
+      '0/2', '1/2', '2/2', '3/2',
+      '0/7', '1/7', '2/7', '3/7'
+    ]
+
+    const expectedFilledState: PositionId[] = [
+      '0/4', '1/4', '2/4', '3/4',
+      '0/5', '1/5', '2/5', '3/5'
+    ]
+
+    const { draft } = getGridStoreInitialStateMock({
+      selectedIds: fillSelection,
+      filledIds: [
+        ...almostFilledLines,
+        ...miscCells
+      ]
+    })
+
+    commitSelectedIds(draft)
+
+    expect(draft.filledIds.sort()).toEqual(expectedFilledState.sort())
+
+    Object.assign(gridConfig, initialGridConfig)
   })
 })
