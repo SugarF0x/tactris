4e243600467764cb6f4572981e9b234952c085b2
Alexey Tuzov <sugarfox.dev@gmail.com>
2022-12-25 15:04:29 +0300

    add getSortedLines tests

---
+++ modules/grid/store/helpers/__tests__/getSortedLines.ts
@@ -0,0 +1,23 @@
+import { CompletionLine } from "~/modules/grid"
+import { getInstructionsMap, getSortedLines } from "~/modules/grid/store/helpers"
+import { Axis } from "~/modules/position"
+
+describe('getSortedLines', () => {
+  it.each<[CompletionLine[], CompletionLine[]]>([
+    [
+      [{ axis: Axis.Y, value: 3 }, { axis: Axis.X, value: 4 }, { axis: Axis.X, value: 5 }, { axis: Axis.Y, value: 6 }],
+      [{ axis: Axis.Y, value: 6 }, { axis: Axis.X, value: 5 }, { axis: Axis.Y, value: 3 }, { axis: Axis.X, value: 4 }]
+    ],
+    [
+      [{ axis: Axis.X, value: 4 }, { axis: Axis.X, value: 3 }, { axis: Axis.X, value: 1 }, { axis: Axis.X, value: 2 }],
+      [{ axis: Axis.X, value: 1 }, { axis: Axis.X, value: 2 }, { axis: Axis.X, value: 3 }, { axis: Axis.X, value: 4 }]
+    ],
+    [
+      [{ axis: Axis.X, value: 8 }, { axis: Axis.X, value: 9 }, { axis: Axis.X, value: 5 }, { axis: Axis.X, value: 6 }],
+      [{ axis: Axis.X, value: 9 }, { axis: Axis.X, value: 8 }, { axis: Axis.X, value: 6 }, { axis: Axis.X, value: 5 }]
+    ],
+  ])('should sort lines inwards according to given instructions', (lines, output) => {
+    const instructions = getInstructionsMap(lines)
+    expect(getSortedLines(lines, instructions)).toMatchObject(output)
+  })
+})
