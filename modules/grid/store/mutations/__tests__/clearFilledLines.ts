a7e5a5ac2bd2ff5b4331d46396a1bcc2514663cb
Alexey Tuzov <sugarfox.dev@gmail.com>
2022-12-25 16:52:02 +0300

    add clearFilledLines tests

---
+++ modules/grid/store/mutations/__tests__/clearFilledLines.ts
@@ -0,0 +1,35 @@
+import { CompletionLine } from "~/modules/grid"
+import { getGridStoreInitialStateMock } from "~/modules/grid/store/__mocks__"
+import { Axis, PositionId } from "~/modules/position"
+import { clearFilledLines } from "~/modules/grid/store/mutations"
+
+describe('clearFilledLines', () => {
+  it.each<[PositionId[], CompletionLine[], PositionId[]]>([
+    [
+      ['1/2'],
+      [{ axis: Axis.X, value: 1 }],
+      []
+    ],
+    [
+      ['2/2', '1/1', '0/0'],
+      [{ axis: Axis.Y, value: 1 }],
+      ['2/2', '0/0']
+    ],
+    [
+      ['2/2', '1/1', '0/0', '3/5', '5/3'],
+      [{ axis: Axis.Y, value: 1 }, { axis: Axis.X, value: 3 }],
+      ['2/2', '0/0', '5/3']
+    ],
+    [
+      ['2/2', '1/1', '0/0', '3/5', '5/3', '7/7'],
+      [{ axis: Axis.Y, value: 1 }, { axis: Axis.X, value: 3 }, { axis: Axis.Y, value: 3 }],
+      ['2/2', '0/0', '7/7']
+    ],
+  ])('should clear IDs for given lines', (filledIds, lines, expected) => {
+    const { draft } = getGridStoreInitialStateMock({ filledIds })
+
+    clearFilledLines(draft, lines)
+
+    expect(draft.filledIds.sort()).toMatchObject(expected.sort())
+  })
+})
