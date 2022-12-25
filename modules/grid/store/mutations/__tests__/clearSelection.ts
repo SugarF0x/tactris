635e33e51ce8e6374ad0b4417ed7e449fd3183b0
Alexey Tuzov <sugarfox.dev@gmail.com>
2022-12-25 17:14:37 +0300

    add clearSelection test

---
+++ modules/grid/store/mutations/__tests__/clearSelection.ts
@@ -0,0 +1,12 @@
+import { getGridStoreInitialStateMock } from "~/modules/grid/store/__mocks__"
+import { clearSelection } from "~/modules/grid/store/mutations"
+
+describe('clearSelection', () => {
+  it('should clear selection', () => {
+    const { draft } = getGridStoreInitialStateMock({ selectedIds: ['1/1', '2/2', '3/3', '4/4'] })
+    expect(draft.selectedIds.length).toBeGreaterThan(0)
+
+    clearSelection(draft)
+    expect(draft.selectedIds).toHaveLength(0)
+  })
+})
