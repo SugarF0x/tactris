ef2f3db1cd5c531b1515f9e0213f5d2772c8f29f
Alexey Tuzov <sugarfox.dev@gmail.com>
2022-12-25 17:16:56 +0300

    add commitSelection test

---
+++ modules/grid/store/mutations/__tests__/commitSelection.ts
@@ -0,0 +1,16 @@
+import { PositionId } from "~/modules/position"
+import { getGridStoreInitialStateMock } from "~/modules/grid/store/__mocks__"
+import { commitSelection } from "~/modules/grid/store/mutations"
+
+describe('commitSelection', () => {
+  it('should add selection ids to filled ids', () => {
+    const filledIds: PositionId[] = ['0/0', '1/1']
+    const selectedIds: PositionId[] = ['2/2', '3/3', '4/4', '5/5']
+
+    const { draft } = getGridStoreInitialStateMock({ filledIds, selectedIds })
+    expect(draft.filledIds).not.toEqual(expect.arrayContaining(selectedIds))
+
+    commitSelection(draft)
+    expect(draft.filledIds).toEqual(expect.arrayContaining(selectedIds))
+  })
+})
