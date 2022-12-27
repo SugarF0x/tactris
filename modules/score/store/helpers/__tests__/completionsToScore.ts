0524b536aabd9d5857e219202adbe345853f35f0
Alexey Tuzov <sugarfox.dev@gmail.com>
2022-12-27 14:14:34 +0300

    add completionsToScore tests

---
+++ modules/score/store/helpers/__tests__/completionsToScore.ts
@@ -0,0 +1,19 @@
+import { mockGridConfig } from "~/modules/grid/__mocks__"
+import { CompletionLine } from "~/modules/grid"
+import { completionsToScore } from "~/modules/score/store/helpers/completionsToScore"
+import { Axis } from "~/modules/position"
+
+describe('completionsToScore', () => {
+  mockGridConfig()
+
+  it.each<[number, number]>([
+    [0, 0],
+    [1, 10],
+    [2, 30],
+    [3, 60],
+    [4, 100],
+  ])('should calculate score based on completions & grid config %d', (completions, output) => {
+    const input: CompletionLine[] = Array(completions).fill({ axis: Axis.X, value: 3 })
+    expect(completionsToScore(input)).toEqual(output)
+  })
+})
--- modules/score/store/helpers/completionsToScore.ts
+++ modules/score/store/helpers/completionsToScore.ts
@@ -3,7 +3,7 @@ import { CompletionLine, axisToGridSizeMap } from "~/modules/grid"
 export function completionsToScore(completionLines: CompletionLine[]): number {
   return completionLines.reduce((acc, line, index) => {
     const baseScorePerLine = axisToGridSizeMap[line.axis]
-    const sequenceMultiplier = .8 * index + .2
+    const sequenceMultiplier = index + 1
 
     return acc + baseScorePerLine * sequenceMultiplier
   }, 0)
