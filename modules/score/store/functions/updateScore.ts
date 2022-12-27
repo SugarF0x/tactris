24ee698f171095316ac821563b601ffa19860b4b
Alexey Tuzov <sugarfox.dev@gmail.com>
2022-12-27 14:15:18 +0300

    use completionsToScore within store function

--- modules/score/store/functions/updateScore.ts
+++ modules/score/store/functions/updateScore.ts
@@ -2,9 +2,10 @@ import { WritableDraft } from "immer/dist/types/types-external"
 import { ScoreStore } from "~/modules/score/store/types"
 import { CompletionLine } from "~/modules/grid"
 import { setHighScore, setScore } from "~/modules/score/store/mutations"
+import { completionsToScore } from "~/modules/score/store/helpers"
 
 export function updateScore(state: WritableDraft<ScoreStore>, lines: CompletionLine[]): void {
-  const value = state.score + lines.reduce((acc, _, index) => acc + 10 * (.8 + index + .2), 0)
+  const value = state.score + completionsToScore(lines)
 
   setScore(state, value)
   setHighScore(state, Math.max(state.highScore, value))
