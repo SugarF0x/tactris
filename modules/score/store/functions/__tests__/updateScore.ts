f1f90104faf8d54717d40b59fb8403a6a364b132
Alexey Tuzov <sugarfox.dev@gmail.com>
2022-12-28 14:56:00 +0300

    add score update function tests
    feeling cute, might update other function tests to look like this

---
+++ modules/score/store/functions/__tests__/updateScore.ts
@@ -0,0 +1,49 @@
+import { getScoreStoreMock } from "~/modules/score/store/__mocks__"
+import { updateScore } from "~/modules/score/store/functions"
+import { mockGridConfig } from "~/modules/grid/__mocks__"
+import { mockCompletionsToScore } from "~/modules/score/store/helpers/__mocks__"
+import { mockSetScore, mockSetHighScore } from "~/modules/score/store/mutations/__mocks__"
+import { CompletionLine } from "~/modules/grid"
+import { Axis } from "~/modules/position"
+
+describe('updateScore', () => {
+  mockGridConfig()
+
+  const completionsToScoreMock = mockCompletionsToScore()
+  const setScoreMock = mockSetScore()
+  const setHighScoreMock = mockSetHighScore()
+
+  it('should calculate additional score and increase score state', () => {
+    const lines: CompletionLine[] = [
+      { axis: Axis.X, value: 3 },
+      { axis: Axis.Y, value: 6 }
+    ]
+
+    const MOCK_HIGH_SCORE_VALUE = 5000
+    const { state, draft } = getScoreStoreMock({ highScore: MOCK_HIGH_SCORE_VALUE })
+
+    updateScore(draft, lines)
+
+    expect(completionsToScoreMock).toHaveBeenCalledWith(lines)
+    expect(setScoreMock).toHaveBeenCalledWith(draft, completionsToScoreMock.mock.results[0].value)
+    expect(setHighScoreMock).toHaveBeenCalledWith(draft, state.highScore)
+  })
+
+  it.skip('should update high score should score be greater than that', () => {
+    const lines: CompletionLine[] = [
+      { axis: Axis.X, value: 3 },
+      { axis: Axis.X, value: 6 },
+      { axis: Axis.Y, value: 3 },
+      { axis: Axis.Y, value: 6 },
+    ]
+
+    const MOCK_SCORE_CALCULATION_RESULT = 30
+    const completionsToScoreMock = mockCompletionsToScore(() => MOCK_SCORE_CALCULATION_RESULT)
+
+    const { draft } = getScoreStoreMock()
+
+    updateScore(draft, lines)
+
+    expect(setHighScoreMock).toHaveBeenCalledWith(draft, completionsToScoreMock.mock.results[0].value)
+  })
+})
