0458619c41a5a004c45b1620fb2d6e7ad2bd810e
Alexey Tuzov <sugarfox.dev@gmail.com>
2022-12-30 16:58:39 +0300

    fix directional floor tests

--- modules/tetra/utils/__tests__/floorTetra.ts
+++ modules/tetra/utils/__tests__/floorTetra.ts
@@ -1,7 +1,11 @@
-import { Position } from "~/modules/position"
-import { floorTetra } from '../floorTetra'
+import { getSize, Position } from "~/modules/position"
+import { FloorCorner, floorTetra } from '../floorTetra'
+import { getSpecificTetra, TetraType } from "~/modules/tetra"
+import { mockGridConfig } from "~/modules/grid/__mocks__"
 
 describe('floorTetra', () => {
+  mockGridConfig()
+
   it.each<[Position[], Position[]]>([
     [
       [{ x: 1, y: 1 }],
@@ -40,6 +44,35 @@ describe('floorTetra', () => {
     expect(output).toEqual(expect.arrayContaining(result))
   })
 
-  it.todo('should floor to appropriate given side at min grid size square for given tetra')
-  it.todo('should floor to appropriate given side with predefined tetra size')
+  describe('directional floor', () => {
+    const tetra = getSpecificTetra(TetraType.L, 2).positions
+
+    it.each<[FloorCorner]>([
+      [FloorCorner.TOP_LEFT],
+      [FloorCorner.TOP_RIGHT],
+      [FloorCorner.BOTTOM_LEFT],
+      [FloorCorner.BOTTOM_RIGHT],
+    ])('should not shift tetra given no specific size override %#', (side) => {
+      expect(floorTetra(tetra, side).sort()).toEqual(tetra.sort())
+    })
+
+    it.each<[FloorCorner]>([
+      [FloorCorner.TOP_LEFT],
+      [FloorCorner.TOP_RIGHT],
+      [FloorCorner.BOTTOM_LEFT],
+      [FloorCorner.BOTTOM_RIGHT],
+    ])('should not move tetra given impossible size options %#', (side) => {
+      expect(floorTetra(tetra, side, getSize(0)).sort()).toEqual(tetra.sort())
+      expect(floorTetra(tetra, side, getSize(1)).sort()).toEqual(tetra.sort())
+    })
+
+    it.each<[FloorCorner, Position, Position[]]>([
+      [FloorCorner.TOP_LEFT, getSize(2), tetra],
+      [FloorCorner.TOP_RIGHT, getSize(4), [{ x: 3, y: 0 }, { x: 4, y: 0 }, { x: 4, y: 1 }, { x: 4, y: 2 }]],
+      [FloorCorner.BOTTOM_LEFT, getSize(7), [{ x: 0, y: 5 }, { x: 1, y: 5 }, { x: 1, y: 6 }, { x: 1, y: 7 }]],
+      [FloorCorner.BOTTOM_RIGHT, getSize(9), [{ x: 8, y: 7 }, { x: 9, y: 7 }, { x: 9, y: 8 }, { x: 9, y: 9 }]],
+    ])('should floor to appropriate given side with predefined tetra size %#', (side, size, output) => {
+      expect(floorTetra(tetra, side, size).sort()).toEqual(output.sort())
+    })
+  })
 })
