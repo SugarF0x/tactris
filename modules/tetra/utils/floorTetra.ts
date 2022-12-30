a7656504c76fa602423000f2c86783c5624d5279
Alexey Tuzov <sugarfox.dev@gmail.com>
2022-12-30 16:58:31 +0300

    fix floor directional calculations

--- modules/tetra/utils/floorTetra.ts
+++ modules/tetra/utils/floorTetra.ts
@@ -9,12 +9,14 @@ export enum FloorCorner {
   BOTTOM_RIGHT = "BOTTOM_RIGHT",
 }
 
-export function floorTetra(tetra: Position[], corner = FloorCorner.TOP_LEFT, size?: number): Position[] {
-  const [minX, minY] = tetra.reduce((acc, val) => {
-    acc[0] = Math.min(val.x, acc[0])
-    acc[1] = Math.min(val.y, acc[1])
+export function floorTetra(tetra: Position[], corner = FloorCorner.TOP_LEFT, size?: Position): Position[] {
+  const [[minX, minY], [maxX, maxY]] = tetra.reduce((acc, val) => {
+    acc[0][0] = Math.min(val.x, acc[0][0])
+    acc[0][1] = Math.min(val.y, acc[0][1])
+    acc[1][0] = Math.max(val.x, acc[1][0])
+    acc[1][1] = Math.max(val.y, acc[1][1])
     return acc
-  }, [GRID_WIDTH, GRID_HEIGHT])
+  }, [[GRID_WIDTH, GRID_HEIGHT], [0, 0]])
 
   const flooredTetra = tetra.map(pos => {
     pos.x -= minX
@@ -22,9 +24,9 @@ export function floorTetra(tetra: Position[], corner = FloorCorner.TOP_LEFT, siz
     return pos
   })
 
-  const flooredSize = size ? size - 1 : Math.max(minX, minY)
-  const shiftX = Math.max(flooredSize - minX, 0)
-  const shiftY = Math.max(flooredSize - minY, 0)
+  const { x: sizeX, y: sizeY } = size ? size : { x: maxX, y: maxY }
+  const shiftX = Math.max(sizeX - maxX, 0)
+  const shiftY = Math.max(sizeY - maxY, 0)
 
   switch (corner) {
     case FloorCorner.TOP_LEFT: return flooredTetra
