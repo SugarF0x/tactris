2cb999a976dc826667f9859379482eacf32218ea
Alexey Tuzov <sugarfox.dev@gmail.com>
2022-12-30 16:18:32 +0300

    add floorTetra corner options

--- modules/tetra/utils/floorTetra.ts
+++ modules/tetra/utils/floorTetra.ts
@@ -1,19 +1,36 @@
 import { Position } from "~/modules/position"
 import { GRID_HEIGHT, GRID_WIDTH } from "~/modules/grid/config"
+import { exhaust } from "~/utils"
 
-// TODO: add option to floor to top/bottom left/right at given grid size
-//  & use it in tetra hud display to space them out evenly to the center
+export enum FloorCorner {
+  TOP_LEFT = "TOP_LEFT",
+  TOP_RIGHT = "TOP_RIGHT",
+  BOTTOM_LEFT = "BOTTOM_LEFT",
+  BOTTOM_RIGHT = "BOTTOM_RIGHT",
+}
 
-export function floorTetra(tetra: Position[]): Position[] {
+export function floorTetra(tetra: Position[], corner = FloorCorner.TOP_LEFT, size?: number): Position[] {
   const [minX, minY] = tetra.reduce((acc, val) => {
     acc[0] = Math.min(val.x, acc[0])
     acc[1] = Math.min(val.y, acc[1])
     return acc
   }, [GRID_WIDTH, GRID_HEIGHT])
 
-  return tetra.map(pos => {
+  const flooredTetra = tetra.map(pos => {
     pos.x -= minX
     pos.y -= minY
     return pos
   })
+
+  const flooredSize = size ? size - 1 : Math.max(minX, minY)
+  const shiftX = flooredSize - minX
+  const shiftY = flooredSize - minY
+
+  switch (corner) {
+    case FloorCorner.TOP_LEFT: return flooredTetra
+    case FloorCorner.TOP_RIGHT: return flooredTetra.map(({ x, y }) => ({ x: x + shiftX, y }))
+    case FloorCorner.BOTTOM_LEFT: return flooredTetra.map(({ x, y }) => ({ x, y: y + shiftY }))
+    case FloorCorner.BOTTOM_RIGHT: return flooredTetra.map(({ x, y }) => ({ x: x + shiftX, y: y + shiftY }))
+    default: exhaust(corner)
+  }
 }
