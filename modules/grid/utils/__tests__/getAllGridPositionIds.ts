69e43a810aa4f66d0b5624b18997e67f051d4bab
Alexey Tuzov <sugarfox.dev@gmail.com>
2022-12-31 15:23:50 +0300

    add getAllGridPositionIds caching tests

--- modules/grid/utils/__tests__/getAllGridPositionIds.ts
+++ modules/grid/utils/__tests__/getAllGridPositionIds.ts
@@ -1,10 +1,26 @@
-import { scopedGridConfigMock } from "~/modules/grid/__mocks__"
+import { mockGridConfig } from "~/modules/grid/__mocks__"
 import { getAllGridPositionIds } from "~/modules/grid/utils"
 
 describe('getAllGridPositionIds', () => {
-  it('should ', () => {
-    scopedGridConfigMock({ GRID_WIDTH: 2, GRID_HEIGHT: 3 }, () => {
-      expect(getAllGridPositionIds().sort()).toEqual(['0/0', '1/0', '0/1', '1/1', '0/2', '1/2'].sort())
+  mockGridConfig({ GRID_WIDTH: 2, GRID_HEIGHT: 3 })
+
+  it('should get all IDs for current grid size config', () => {
+    expect(getAllGridPositionIds().sort()).toEqual(['0/0', '1/0', '0/1', '1/1', '0/2', '1/2'].sort())
+  })
+
+  it('should return cache if function has already done calculations', () => {
+    jest.isolateModules(() => {
+      const { getAllGridPositionIds } = require("~/modules/grid/utils")
+
+      const pushMock = jest.fn(Array.prototype.push)
+      jest.spyOn(Array.prototype, 'push').mockImplementation(pushMock)
+
+      getAllGridPositionIds()
+      expect(pushMock).toHaveBeenCalled()
+
+      pushMock.mockClear()
+      getAllGridPositionIds()
+      expect(pushMock).not.toHaveBeenCalled()
     })
   })
 })
