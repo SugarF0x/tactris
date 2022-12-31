e9871205f054d10d9de540a457349822b772eadb
Alexey Tuzov <sugarfox.dev@gmail.com>
2022-12-31 14:19:06 +0300

    add getTetraSize tests

---
+++ modules/tetra/utils/__tests__/getTetraSize.ts
@@ -0,0 +1,14 @@
+import { Position } from "~/modules/position"
+import { getTetraSize } from "~/modules/tetra/utils/getTetraSize"
+import { getSpecificTetra, TetraType } from "~/modules/tetra"
+
+describe('getTetraSize', () => {
+  it.each<[TetraType, Position]>([
+    [TetraType.L, { x: 1, y: 2 }],
+    [TetraType.T, { x: 2, y: 1 }],
+    [TetraType.O, { x: 1, y: 1 }],
+    [TetraType.I, { x: 0, y: 3 }],
+  ])('should calculate given tetra size %#', (input, output) => {
+    expect(getTetraSize(getSpecificTetra(input).positions)).toEqual(output)
+  })
+})
