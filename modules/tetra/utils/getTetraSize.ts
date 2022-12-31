633d8c5022a77958ee16e07004bd4c581b15e8e2
Alexey Tuzov <sugarfox.dev@gmail.com>
2022-12-31 14:12:56 +0300

    add getTetraSize util

---
+++ modules/tetra/utils/getTetraSize.ts
@@ -0,0 +1,9 @@
+import { Position } from "~/modules/position"
+
+export function getTetraSize(tetra: Position[]): Position {
+  return tetra.reduce((acc, { x, y }) => {
+    acc.x = Math.max(acc.x, x)
+    acc.y = Math.max(acc.y, y)
+    return acc
+  }, { x: -1, y: -1 })
+}
