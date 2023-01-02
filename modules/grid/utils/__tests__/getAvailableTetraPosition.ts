587c0cc58596de54f0a3b31be24f6fa6a798431f
Alexey Tuzov <sugarfox.dev@gmail.com>
2023-01-02 13:25:47 +0300

    add getAvailableTetraPosition tests

---
+++ modules/grid/utils/__tests__/getAvailableTetraPosition.ts
@@ -0,0 +1,25 @@
+import { getSpecificTetra, TetraType } from "~/modules/tetra"
+import { PositionId } from "~/utils"
+import { mockGridConfig } from "~/modules/grid/__mocks__"
+import { getAvailableTetraPosition } from "~/modules/grid/utils/getAvailableTetraPosition"
+
+describe('getAvailableTetraPosition', () => {
+  mockGridConfig({ GRID_WIDTH: 4, GRID_HEIGHT: 4 })
+
+  it.each<[TetraType, PositionId[]]>([
+    [TetraType.I, ['0/0','1/0','2/0','3/0']],
+    [TetraType.L, ['0/1','1/2','2/3','3/0', '2/0']],
+    [TetraType.O, ['1/1','3/1','1/3', '3/3']],
+  ])('should return null when no position is available %#', (type, filled) => {
+    expect(getAvailableTetraPosition(getSpecificTetra(type).positions, filled)).toBeNull()
+  })
+
+  it.each<[TetraType, PositionId[], PositionId[]]>([
+    [TetraType.I, [], ['0/0','0/1','0/2','0/3']],
+    [TetraType.I, ['1/1'], ['0/0','0/1','0/2','0/3']],
+    [TetraType.I, ['0/0'], ['1/0','1/1','1/2','1/3']],
+    [TetraType.O, ['0/1','0/2','3/1','3/2','2/0','3/3'], ['1/1','1/2','2/1','2/2']],
+  ])('should return first encountered possible position ids %#', (type, filled, output) => {
+    expect(getAvailableTetraPosition(getSpecificTetra(type).positions, filled)?.sort()).toEqual(output.sort())
+  })
+})
