5bcccfb9b320e244def8be586d6e145931ae5a58
Alexey Tuzov <sugarfox.dev@gmail.com>
2023-01-02 13:49:49 +0300

    add available moves hook

---
+++ modules/grid/ui/Grid/hooks/useAvailableMoves.ts
@@ -0,0 +1,33 @@
+import { useGridStore } from "~/modules/grid"
+import { TetraObject } from "~/modules/tetra"
+import { useMemo } from "react"
+import { PositionId } from "~/utils"
+import { getAvailableTetraPosition } from "~/modules/grid/utils/getAvailableTetraPosition"
+
+export interface TetraGridState extends TetraObject {
+  canPlace: boolean
+}
+
+export function useAvailableMoves(): [TetraGridState, TetraGridState] {
+  const tetras = useGridStore(state => state.tetras)
+  const filledIds = useGridStore(state => state.filledIds)
+
+  const tetraToAvailablePositionsWeakMap = useMemo(() => {
+    const map = new WeakMap<TetraObject, PositionId[]>()
+
+    for (const tetra of tetras) {
+      const availablePosition = getAvailableTetraPosition(tetra.positions, filledIds)
+      if (!availablePosition) continue
+      map.set(tetra, availablePosition)
+    }
+
+    return map
+  }, [tetras, filledIds])
+
+  return useMemo(() => tetras.map(tetra => {
+    return {
+      ...tetra,
+      canPlace: tetraToAvailablePositionsWeakMap.has(tetra)
+    }
+  }) as [TetraGridState, TetraGridState], [tetras, tetraToAvailablePositionsWeakMap])
+}
