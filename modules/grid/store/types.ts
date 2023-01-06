9c59f4f438655657d95b98cb35d5147f0f143e41
Alexey Tuzov <sugarfox.dev@gmail.com>
2023-01-06 20:40:10 +0300

    update store state with grid reserve tetra

--- modules/grid/store/types.ts
+++ modules/grid/store/types.ts
@@ -1,10 +1,15 @@
 import { PositionId } from "~/utils"
 import { TetraObject } from "~/modules/tetra"
 
+export interface GridTetras {
+  available: [TetraObject, TetraObject]
+  reserve: TetraObject
+}
+
 export interface GridStore {
   filledIds: PositionId[]
   selectedIds: PositionId[]
-  tetras: [TetraObject, TetraObject]
+  tetras: GridTetras
   selectId: (id: PositionId) => void
   commitSelectedIds: () => void
   restart: () => void
