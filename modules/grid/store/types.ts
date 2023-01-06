c1547b394ded9936aac24005dc3c33fbdb1d6494
Alexey Tuzov <sugarfox.dev@gmail.com>
2023-01-06 20:35:09 +0300

    Revert "set store tetras field to have 3 tetras total"
    This reverts commit 21340ed77354eb779285459e32fd4ba407495d79.

--- modules/grid/store/types.ts
+++ modules/grid/store/types.ts
@@ -4,7 +4,7 @@ import { TetraObject } from "~/modules/tetra"
 export interface GridStore {
   filledIds: PositionId[]
   selectedIds: PositionId[]
-  tetras: [TetraObject, TetraObject, TetraObject]
+  tetras: [TetraObject, TetraObject]
   selectId: (id: PositionId) => void
   commitSelectedIds: () => void
   restart: () => void
