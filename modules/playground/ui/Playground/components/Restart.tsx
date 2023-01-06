69d1a87bc9e4ddb9149c3293a0e3b975169fe916
Alexey Tuzov <sugarfox.dev@gmail.com>
2023-01-06 20:04:38 +0300

    allow midgame restarts

--- modules/playground/ui/Playground/components/Restart.tsx
+++ modules/playground/ui/Playground/components/Restart.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable react/jsx-no-literals */
 import React from 'react'
 import { Button } from "~/components"
-import { Text } from "react-native"
+import { Alert, Text } from "react-native"
 import { cyan } from "~/styles"
 import { useAvailableMoves } from "~/modules/grid/ui/Grid/hooks"
 import { useScoreStore } from "~/modules/score"
@@ -9,18 +9,38 @@ import { useGridStore } from "~/modules/grid"
 
 export function Restart() {
   const tetras = useAvailableMoves()
-  const canRestart = tetras.every(tetra => !tetra.canPlace)
+  const shouldRestart = tetras.every(tetra => !tetra.canPlace)
 
   const restartScore = useScoreStore(state => state.restart)
   const restartGrid = useGridStore(state => state.restart)
 
-  function handleRestart() {
+  function restart() {
     restartGrid()
     restartScore()
   }
 
+  function handleRestart() {
+    if (shouldRestart) return restart()
+
+    Alert.alert(
+      "Restart",
+      "You are about to restart an ongoing game with remaining possible moves, are you sure you want to restart?",
+      [
+        {
+          text: "Cancel",
+          style: "cancel",
+        },
+        {
+          text: "Restart",
+          onPress: restart,
+          style: "default",
+        },
+      ]
+    )
+  }
+
   return (
-    <Button disabled={!canRestart} onPress={handleRestart}>
+    <Button onPress={handleRestart}>
       <Text style={{ color: cyan }}>
         Restart
       </Text>
