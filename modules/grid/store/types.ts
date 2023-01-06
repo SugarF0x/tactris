21340ed77354eb779285459e32fd4ba407495d79
Alexey Tuzov <sugarfox.dev@gmail.com>
2023-01-06 20:09:11 +0300

    set store tetras field to have 3 tetras total

--- modules/grid/store/types.ts
+++ modules/grid/store/types.ts
@@ -4,7 +4,7 @@ import { TetraObject } from "~/modules/tetra"
 export interface GridStore {
   filledIds: PositionId[]
   selectedIds: PositionId[]
-  tetras: [TetraObject, TetraObject]
+  tetras: [TetraObject, TetraObject, TetraObject]
   selectId: (id: PositionId) => void
   commitSelectedIds: () => void
   restart: () => void
