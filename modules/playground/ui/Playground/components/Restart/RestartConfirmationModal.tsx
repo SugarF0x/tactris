ce1594c130096ecee98ef05f11fb4ee4a112120b
Alexey Tuzov <sugarfox.dev@gmail.com>
2023-01-29 11:03:45 +0300

    style confirmation modal

--- modules/playground/ui/Playground/components/Restart/RestartConfirmationModal.tsx
+++ modules/playground/ui/Playground/components/Restart/RestartConfirmationModal.tsx
@@ -6,6 +6,8 @@ import { useScoreStore } from "~/modules/score"
 import { useGridStore } from "~/modules/grid"
 import { Button, Card } from "~/components"
 import { useMountEffect } from "~/hooks"
+import { opacify } from "~/utils"
+import { cyan, Fonts } from "~/styles"
 
 export interface RestartConfirmationModalProps {
   open: boolean
@@ -37,18 +39,22 @@ export function RestartConfirmationModal(props: RestartConfirmationModalProps) {
       transparent
     >
       <View style={styles.wrapper}>
-        <Card>
+        <Card style={styles.card}>
           <Text style={styles.title}>Restart</Text>
 
           <Text style={styles.body}>You are about to restart an ongoing game with remaining possible moves, are you sure you want to restart?</Text>
 
           <View style={styles.actions}>
-            <Button onPress={onClose}>
-              No
-            </Button>
-            <Button onPress={restart}>
-              Yes
-            </Button>
+            <Card style={styles.buttonWrapper}>
+              <Button onPress={onClose} style={styles.button}>
+                No
+              </Button>
+            </Card>
+            <Card style={[styles.buttonWrapper, styles.spaced]}>
+              <Button onPress={restart} style={styles.button}>
+                Yes
+              </Button>
+            </Card>
           </View>
         </Card>
       </View>
@@ -58,14 +64,41 @@ export function RestartConfirmationModal(props: RestartConfirmationModalProps) {
 
 const styles = StyleSheet.create({
   wrapper: {
+    backgroundColor: opacify('#000000', .5),
     flex: 1,
     justifyContent: 'center',
-    padding: 24
+    paddingHorizontal: 56
   },
-  title: {},
-  body: {},
+  card: {
+    overflow: 'hidden'
+  },
+  title: {
+    fontFamily: Fonts.MAIN_BOLD,
+    color: cyan,
+    textTransform: 'uppercase',
+    textAlign: 'center',
+    fontSize: 24
+  },
+  body: {
+    fontFamily: Fonts.MAIN,
+    textAlign: 'center',
+    color: 'white',
+    fontSize: 16
+  },
   actions: {
     flexDirection: 'row',
-    justifyContent: 'space-evenly'
+    marginTop: 8,
+  },
+  buttonWrapper: {
+    flex: 1,
+    paddingVertical: 6,
+    paddingHorizontal: 6
+  },
+  button: {
+    paddingVertical: 2,
+    paddingHorizontal: 4,
+  },
+  spaced: {
+    marginLeft: 16
   }
 })
