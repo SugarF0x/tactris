610e976684a5b0f69224930bc21a4f2837fae06f
Alexey Tuzov <sugarfox.dev@gmail.com>
2023-01-30 10:14:58 +0300

    properly hide android bottom nav bar

--- App.tsx
+++ App.tsx
@@ -4,10 +4,17 @@ import * as SplashScreen from 'expo-splash-screen'
 import { StyleSheet, View } from "react-native"
 import { useFonts } from "expo-font"
 import { Fonts } from "~/styles"
+import { setBehaviorAsync, setVisibilityAsync } from 'expo-navigation-bar'
+import { useMountEffect } from "~/hooks"
 
 void SplashScreen.preventAutoHideAsync()
 
 export default function App() {
+  useMountEffect(() => {
+    void setVisibilityAsync('hidden')
+    void setBehaviorAsync('overlay-swipe')
+  })
+
   const [fontsLoaded] = useFonts({
     [Fonts.MAIN]: require('~/assets/fonts/ChakraPetch-Regular.ttf'),
     [Fonts.MAIN_BOLD]: require('~/assets/fonts/ChakraPetch-Bold.ttf'),
