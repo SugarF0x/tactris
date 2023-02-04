73e5b04a8a4d934d1d0b25334e3f4a6b87098d48
Alexey Tuzov <sugarfox.dev@gmail.com>
2023-02-04 16:21:40 +0300

    adjust shadows a bit more

--- styles/shadows.ts
+++ styles/shadows.ts
@@ -2,8 +2,8 @@ import { cyan } from "~/styles/colors"
 import { generateBoxShadowStyle } from "~/utils"
 
 export const cyanShadow = generateBoxShadowStyle({
-  shadowRadius: 25,
+  shadowRadius: 20,
   shadowOpacity: .5,
-  elevation: 32,
+  elevation: 24,
   shadowColor: cyan,
 })
