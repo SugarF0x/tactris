4dcab61fb2d224fe7dd9327a82fe1556603f725e
Alexey Tuzov <sugarfox.dev@gmail.com>
2023-02-12 17:26:21 +0300

    Revert "update zustand imports"
    This reverts commit fb4f58954ff6e9b32c9dd5798f9bd708d9af7bc0.

--- services/undo/store.ts
+++ services/undo/store.ts
@@ -1,4 +1,4 @@
-import { create } from "zustand"
+import create from "zustand"
 import { createJSONStorage, persist } from "zustand/middleware"
 import { immer } from "zustand/middleware/immer"
 import AsyncStorage from "@react-native-async-storage/async-storage"
