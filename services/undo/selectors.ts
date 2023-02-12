0383fee8a87879ce416ed815d3abfcdf869828e6
Alexey Tuzov <sugarfox.dev@gmail.com>
2023-02-12 17:26:21 +0300

    Revert "add skeleton undo service"
    This reverts commit 8bbfbd58796d68f5c81de7a67b97b44422315b3d.

--- services/undo/selectors.ts
+++
@@ -1,6 +0,0 @@
-import { UndoStore } from "~/services/undo/types"
-
-const select = <T>(selector: (state: UndoStore) => T) => selector
-
-export const selectCanUndo = select(state => state.items.every(Boolean))
-export const selectUndoCost = select(state => Math.min(state.totalUndos * 4, 9999))
